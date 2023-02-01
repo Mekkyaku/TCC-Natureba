a84b7ca516d2eac4cc6b469b75f4f12a088ceb8f
Igor Henrique <80785471+Mekkyaku@users.noreply.github.com>
2023-01-31 22:30:58 -0300

    Delete NaturebaTCC directory

--- NaturebaTCC/felp/html/conta/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if ((isset($_SESSION["logado"]) && ($_SESSION["logado"] == TRUE))){
-        session_destroy();
-    }
-
-    header("Location: login.php");
-?>
--- NaturebaTCC/html/account/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if ((isset($_SESSION["login"]) && ($_SESSION["login"] == TRUE))){
-        session_destroy();
-    }
-
-    header("Location: ../../index.html");
-?>
